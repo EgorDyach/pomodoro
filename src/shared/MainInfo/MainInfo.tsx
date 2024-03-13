0e54a4c8416fba5b514de7e05521a2cf4869af97
EgorDyach <tvoysosed33@gmail.com>
2024-03-13 23:16:19 +0300

    Added totalTime

--- src/shared/MainInfo/MainInfo.tsx
+++ src/shared/MainInfo/MainInfo.tsx
@@ -3,33 +3,77 @@ import { Text } from '../../components/Text';
 import { MainInfoForm } from './MainInfoForm';
 import { MainList } from './MainList';
 import './maininfo.css';
-import { RootState } from '../../store/store';
+import { RootState, Task } from '../../store/store';
+
+
+const TIME_OF_TASK = 25
 
 export function MainInfo() {
+  let sumOfMinutes = 0
   const appTheme = useSelector<RootState, string>(state => state.Local.appTheme)
+  const tasks = useSelector<RootState, Task[]>(state => state.Local.arrayOfTasks);
+  tasks.forEach((e) => {
+    sumOfMinutes += (e.count * TIME_OF_TASK)
+  })
+  let isMore = false;
+  let countOfHours = Math.floor(sumOfMinutes / 60);
+  let wordOfHours = '';
+  let countOfMinutes = sumOfMinutes - Math.floor(sumOfMinutes / 60) * 60;
+  if (countOfHours > 99) {
+    countOfHours = 99
+    countOfMinutes = 0
+    isMore = true;
+  } 
+  if (countOfHours) {
+    if (countOfHours === 1) {
+      wordOfHours = 'час'
+    } else if (countOfHours >= 5) {
+      wordOfHours = 'часов'
+    } else {
+      wordOfHours = 'часа'
+    }
+  }
+  let wordOfMinutes = '';
+  console.log(countOfMinutes.toString().slice(1))
+  if (countOfMinutes) {
+    if (countOfMinutes.toString().slice(1) === '1') {
+      wordOfMinutes = 'минута'
+    } else if (Number(countOfMinutes.toString().slice(1)) >= 5 || Number(countOfMinutes.toString().slice(1)) === 0) {
+      wordOfMinutes = 'минут'
+    } else {
+      wordOfMinutes = 'минуты'
+    }
+  }
   return (
     <div className='mainInfo'>
-      <Text weight={700} color={appTheme === 'dark' ? "#E7E7E7" :'#333333'} size={24} className='mainInfo__title'>Ура! Теперь можно начать работать:</Text>
+      <Text weight={700} color={appTheme === 'dark' ? "#E7E7E7" : '#333333'} size={24} className='mainInfo__title'>Ура! Теперь можно начать работать:</Text>
       <ul className='mainInfo__list'>
         <li className='mainInfo__item'>
-          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" :'#333333'} size={16} className='mainInfo__item-text'>Выберите категорию и напишите название текущей задачи</Text>
+          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" : '#333333'} size={16} className='mainInfo__item-text'>Выберите категорию и напишите название текущей задачи</Text>
         </li>
         <li className='mainInfo__item'>
-          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" :'#333333'} size={16} className='mainInfo__item-text'>Запустите таймер («помидор»)</Text>
+          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" : '#333333'} size={16} className='mainInfo__item-text'>Запустите таймер («помидор»)</Text>
         </li>
         <li className='mainInfo__item'>
-          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" :'#333333'} size={16} className='mainInfo__item-text'>Работайте пока «помидор» не прозвонит</Text>
+          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" : '#333333'} size={16} className='mainInfo__item-text'>Работайте пока «помидор» не прозвонит</Text>
         </li>
         <li className='mainInfo__item'>
-          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" :'#333333'} size={16} className='mainInfo__item-text'>Сделайте короткий перерыв (3-5 минут)</Text>
+          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" : '#333333'} size={16} className='mainInfo__item-text'>Сделайте короткий перерыв (3-5 минут)</Text>
         </li>
         <li className='mainInfo__item'>
-          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" :'#333333'} size={16} className='mainInfo__item-text'>Продолжайте работать «помидор» за «помидором», пока задача не будут выполнена. Каждые 4 «помидора» делайте длинный перерыв (15-30 минут).</Text>
+          <Text weight={300} color={appTheme === 'dark' ? "#E7E7E7" : '#333333'} size={16} className='mainInfo__item-text'>Продолжайте работать «помидор» за «помидором», пока задача не будут выполнена. Каждые 4 «помидора» делайте длинный перерыв (15-30 минут).</Text>
         </li>
       </ul>
       <div className='mainInfo__tasks'>
         <MainInfoForm />
         <MainList />
+        <p className='mainInfo__totalTime'>
+          {countOfHours !== 0 && countOfHours} {wordOfHours !== '' && wordOfHours+' '} 
+           
+          {countOfMinutes !== 0 && countOfMinutes} {wordOfMinutes !== '' && wordOfMinutes}
+          {isMore && 'и более...'}
+          {/* {sumOfMinutes - Math.floor(sumOfMinutes / 60) * 60} минут */}
+        </p>
       </div>
     </div>
   );
